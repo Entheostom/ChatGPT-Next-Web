e037a7c874f1840a1bf1b73e0fcf5ec87956242d
Entheostom <158443908+Entheostom@users.noreply.github.com>
2024-06-06 14:51:33 +0800

    cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -377,8 +377,8 @@ const cn = {
   },
   Store: {
     DefaultTopic: "新的聊天",
-    BotHello: "有什么可以帮你的吗",
-    Error: "出错了，稍后重试吧",
+    BotHello: "小猫咪，有什么可以帮你的吗",
+    Error: "错了错了，稍后重试吧",
     Prompt: {
       History: (content: string) => "这是历史聊天总结作为前情提要：" + content,
       Topic:
